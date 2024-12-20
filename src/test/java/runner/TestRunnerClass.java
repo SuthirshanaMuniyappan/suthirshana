e9fbe400b77a14b6b5d4e6adb8c2d4e104542a18
Suthir <suthirshana.muniyappan@expleogroup.com>
2024-12-20 14:30:24 +0530

    Added

--- src/test/java/runner/TestRunnerClass.java
+++ src/test/java/runner/TestRunnerClass.java
@@ -7,13 +7,14 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "C:\\Users\\TVDI-00\\eclipse-workspace\\mavenProject\\"
-		+ "src\\test\\resources\\LoginPage.feature", 
-		glue = {"stepDef"},
-		plugin = {"pretty", "html:target/result.html"},
-		dryRun = true
-		
-		) 
+@CucumberOptions(
+				features = "C:\\Users\\TVDI-00\\eclipse-workspace\\mavenProject\\"
+				+ "src\\test\\resources\\LoginPage.feature", 
+				glue = {"stepDef"},
+				plugin = {"pretty", "html:target/result.html"},
+				dryRun = false
+				
+				) 
 public class TestRunnerClass {
 
 	
